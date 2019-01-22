644b4fc00e4928443b14bbf377ccb3e066f8158d
sabbir073 <md.sabbir073@gmail.com>
2019-01-22 06:23:24 +0600

    Updates

---
+++ precache-manifest.5273901c03b2cdca0bf75c5c8998ac4f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9650ee734cb54c58e21b7769f369c2b3",
+    "url": "/react-website-template/static/media/mokup-02.9650ee73.png"
+  },
+  {
+    "revision": "06d7dc0b7c70323ab4f1",
+    "url": "/react-website-template/static/js/main.06d7dc0b.chunk.js"
+  },
+  {
+    "revision": "592bd51fe5f295f77e70a07ed677fa9c",
+    "url": "/react-website-template/static/media/team-01.592bd51f.png"
+  },
+  {
+    "revision": "62db135427c0da2ff7c3",
+    "url": "/react-website-template/static/js/runtime~main.62db1354.js"
+  },
+  {
+    "revision": "87c5ec2ea5077d7395fa91ecc6bbe070",
+    "url": "/react-website-template/static/media/slider-inner-02.87c5ec2e.png"
+  },
+  {
+    "revision": "96d76f18ce4a2ee1b6714a5ffcb61871",
+    "url": "/react-website-template/static/media/mokup-07.96d76f18.png"
+  },
+  {
+    "revision": "c819542042cea50e347d9eb8d1a02db3",
+    "url": "/react-website-template/static/media/01.c8195420.jpg"
+  },
+  {
+    "revision": "173cc4513f8a38f80095088bc1484976",
+    "url": "/react-website-template/static/media/mokup-03.173cc451.png"
+  },
+  {
+    "revision": "88cd720e71d329645781e45448945e7a",
+    "url": "/react-website-template/static/media/mokup-05.88cd720e.png"
+  },
+  {
+    "revision": "5e0560c356da1d7d81b6c7a0c871a7e0",
+    "url": "/react-website-template/static/media/mokup-06.5e0560c3.png"
+  },
+  {
+    "revision": "50d3b7aa0876bdfde1a7",
+    "url": "/react-website-template/static/js/1.50d3b7aa.chunk.js"
+  },
+  {
+    "revision": "50d3b7aa0876bdfde1a7",
+    "url": "/react-website-template/static/css/1.f2e9f258.chunk.css"
+  },
+  {
+    "revision": "debc36c373d14b14ba440065030a79ad",
+    "url": "/react-website-template/index.html"
+  }
+];
